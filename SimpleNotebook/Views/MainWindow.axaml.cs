d828a9c70e5358d05ef675bff52fe23abe20099d
Самарин Даниил <daniil_samarin@mail.ru>
2023-03-12 17:28:53 +0700

    Добавьте файлы проекта.

---
+++ SimpleNotebook/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SimpleNotebook.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
